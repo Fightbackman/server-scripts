f0f8c43ab02f089862883fcde88043402763b55d
Fightbackman <fightbackman@firepulse.de>
2016-05-20 22:33:51 +0200

    Fixed sed, but it's static now. Documented progress in README

--- letsencrypt/addnew/new_domain.sh
+++ letsencrypt/addnew/new_domain.sh
@@ -10,10 +10,11 @@ if [ ! -f $config_file ]; then
 echo "Config file does not exist"
 exit 1;
 fi
-config_file="/etc/letsencrypt/configs/$configname
+config_file="/etc/letsencrypt/configs/$configname"
 echo "The Used config file is: $config_file"
 echo "Adding domain to existing config."
-sed "/$configname/s/$/ $newdomain/" $config_file
+sed "16s/$/, $newdomain/" $config_file > $config_file.tmp
+mv $config_file.tmp $config_file
 echo "Shutdown of $web_service"
 /usr/sbin/service $web_service stop
 echo "Aquiring new certificate"
