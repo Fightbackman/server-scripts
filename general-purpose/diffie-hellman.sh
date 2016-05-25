b49cab9c77625d4f5c215c2d70e35a994a07fb7f
Kevin <kevin.diehl@rwth-aachen.de>
2016-05-26 00:15:17 +0200

    fixed path of diffie-hellman-background

--- general-purpose/diffie-hellman.sh
+++ general-purpose/diffie-hellman.sh
@@ -8,4 +8,4 @@ scp ../general-purpose/diffie-hellman-background.sh $USER@$IP:~/;
 
 ssh -t $USER@$IP 'sudo apt-get install curl;';
 
-ssh -t $USER@$IP "sudo su -c \"chmod +x /home/$USER/diffie-hellman-background.sh; nohup /home/$USER/diffie-hellman-background.sh > ~diffie.log 2>&1 & exit\" "
+ssh -t $USER@$IP "sudo su -c \"chmod +x /home/$USER/diffie-hellman-background.sh; nohup /home/$USER/diffie-hellman-background.sh > ~/diffie.log 2>&1 & exit\" "
