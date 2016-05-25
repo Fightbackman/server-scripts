648ce9f9ac99fd9dfc2ba36f40881571c39b8c94
Kevin <kevin.diehl@rwth-aachen.de>
2016-05-26 01:22:42 +0200

    bugfix in teamspeak3 installation script

--- software/teamspeak3.sh
+++ software/teamspeak3.sh
@@ -9,7 +9,7 @@ ssh -t $USER@$IP '
   sudo su teamspeak3 -c "
     cd /usr/local/teamspeak3;
     wget http://dl.4players.de/ts/releases/3.0.12.4/teamspeak3-server_linux_amd64-3.0.12.4.tar.bz2;
-    tar -xzvf teamspeak3-server_linux*.tar.gz;
-    /usr/local/teamspeak3/teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh;
+    tar -xvjf teamspeak3-server_linux_amd64-3.0.12.4.tar.bz2;
+    /usr/local/teamspeak3/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh;
     /usr/local/teamspeak3/teamspeak3-server_linux-amd64/ts3server_startscript.sh start;";';
     #last command ist to start the server
