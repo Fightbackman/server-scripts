9f259d4113f7c83147b9afb90eefc71748de94db
Kevin <kevin.diehl@rwth-aachen.de>
2016-05-26 18:31:54 +0200

    something misterious is going on... manual editing of /etc/apt/sources.list.d/docker.list works but ">>" wont

--- software/docker.sh
+++ software/docker.sh
@@ -11,7 +11,7 @@ ssh -t $USER@$IP '
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D;
   sudo rm /etc/apt/sources.list.d/docker.list;
   sudo touch /etc/apt/sources.list.d/docker.list;
-  sudo echo "deb https://apt.dockerproject.org/repo debian-jessie main" >> /etc/apt/sources.list.d/docker.list;
+  sudo echo "deb https://apt.dockerproject.org/repo debian-jessie main >> /etc/apt/sources.list.d/docker.list"; #hier kommen misterioese probleme auf
   sudo apt-get update;
   sudo apt-cache policy docker-engine;
   sudo apt-get update;
