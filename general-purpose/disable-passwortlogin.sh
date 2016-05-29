f0dbae91efa0a9ae9fce890535c5eabf0cb1527a
Fightbackman <fightbackman@firepulse.de>
2016-05-29 12:23:50 +0200

    moved usercreation to if block

--- general-purpose/disable-passwortlogin.sh
+++ general-purpose/disable-passwortlogin.sh
@@ -9,13 +9,14 @@ echo "KEEP THIS PASSWORD SECRET. $FALLBACK HAS UNLIMITED ACCCESS TO YOUR SYSTEM!
 wait(5000)
 ssh -t $USER@$IP "
 if id $FALLBACK > /dev/null 2>&1; then
-  echo 'user already exists';
-  exit 1;
-fi
-sudo adduser $FALLBACK --home /home/$FALLBACK --gecos '' --shell /bin/bash --disabled-password;
-echo $FALLPWD | sudo passwd $FALLBACK --stdin;
+  echo ''
+else
+  sudo adduser $FALLBACK --home /home/$FALLBACK --gecos '' --shell /bin/bash --disabled-password;
+  echo $FALLPWD | sudo passwd $FALLBACK --stdin;
+fi;
 echo '$FALLBACK  ALL=(ALL:ALL) ALL' >> /etc/sudoers;
 "
 ssh -t $USER@$IP s
 sudo  printf '%s\n %s\n' 'Match User !$FALLBACK' 'Password Authentication no' >> etc/ssh/sshd_config;
   service ssh restart";
+exit 0;
