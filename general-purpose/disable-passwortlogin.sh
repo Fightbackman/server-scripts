3fa2997d3cace1100835b2232c4e7424d07ce7d6
Fightbackman <fightbackman@firepulse.de>
2016-05-29 12:21:28 +0200

    added check if fallbackusername is already in use
    If the unsername already exists. This user will be defined as
    fallbackuser in sshd_conf

--- general-purpose/disable-passwortlogin.sh
+++ general-purpose/disable-passwortlogin.sh
@@ -7,7 +7,12 @@ echo "generating fallback user with Username $FALLBACK and password $FALLPWD."
 echo "adding user $FALLBACK to sudoers file."
 echo "KEEP THIS PASSWORD SECRET. $FALLBACK HAS UNLIMITED ACCCESS TO YOUR SYSTEM!"
 wait(5000)
-ssh -t $USER@$IP "sudo adduser $FALLBACK --home /home/$FALLBACK --gecos '' --shell /bin/bash --disabled-password;
+ssh -t $USER@$IP "
+if id $FALLBACK > /dev/null 2>&1; then
+  echo 'user already exists';
+  exit 1;
+fi
+sudo adduser $FALLBACK --home /home/$FALLBACK --gecos '' --shell /bin/bash --disabled-password;
 echo $FALLPWD | sudo passwd $FALLBACK --stdin;
 echo '$FALLBACK  ALL=(ALL:ALL) ALL' >> /etc/sudoers;
 "
