9a728048de83d7bb99349bda73eb090ba1e692e3
Fightbackman <fightbackman@firepulse.de>
2016-05-29 12:17:59 +0200

    Added script to disable passwort login for all users, except one newly created fallbackuser

---
+++ general-purpose/disable-passwortlogin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+$USER = $1
+$IP = $2
+$FALLBACK = $3
+$FALLPWD = $4
+echo "generating fallback user with Username $FALLBACK and password $FALLPWD."
+echo "adding user $FALLBACK to sudoers file."
+echo "KEEP THIS PASSWORD SECRET. $FALLBACK HAS UNLIMITED ACCCESS TO YOUR SYSTEM!"
+wait(5000)
+ssh -t $USER@$IP "sudo adduser $FALLBACK --home /home/$FALLBACK --gecos '' --shell /bin/bash --disabled-password;
+echo $FALLPWD | sudo passwd $FALLBACK --stdin;
+echo '$FALLBACK  ALL=(ALL:ALL) ALL' >> /etc/sudoers;
+"
+ssh -t $USER@$IP s
+sudo  printf '%s\n %s\n' 'Match User !$FALLBACK' 'Password Authentication no' >> etc/ssh/sshd_config;
+  service ssh restart";
