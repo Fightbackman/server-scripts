a64ce79abf2ead0bf93b7269ba5b87ed8ac3131c
Kevin <kevin.diehl@rwth-aachen.de>
2016-06-15 18:25:53 +0200

    added script for creation of sftp users with restricted access and creation of webfolders. no subdomain creation included

---
+++ general-purpose/add_sftp_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USER=$1;
+IP=$2;
+newUser=$3;
+DOMAIN=$4;
+
+ssh -t $USER@$IP "
+  sudo adduser $newUser;
+  sudo usermod -G sftp $newUser;
+  sudo usermod -s /bin/false $newUser;
+  sudo chown root:root /home/$newUser;
+  sudo mkdir /home/$newUser/web;
+  sudo chown $newUser:$newUser /home/$newUser/web;
+  sudo ln -s /home/$newUser/web /var/www/html/$DOMAIN;"
