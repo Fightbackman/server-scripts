45903a8d36a2f3005e87d34855f0e27eb42fa6ff
Fightbackman <fightbackman@firepulse.de>
2016-06-18 22:08:39 +0200

    Fixed missing folder error in install_letsencrypt.sh

--- software/install_letsencrypt.sh
+++ software/install_letsencrypt.sh
@@ -9,4 +9,6 @@ ssh -t $USER@$IP '
   sudo apt-get -y install git;
   cd /opt/;
   sudo git clone https://github.com/certbot/certbot;
-  sudo mkdir /etc/letsencrypt/configs/;'
+  sudo mkdir /etc/letsencrypt;
+  sudo mkdir /etc/letsencrypt/configs/;
+  sudo chown -R root:root /etc/letsencrypt;'
