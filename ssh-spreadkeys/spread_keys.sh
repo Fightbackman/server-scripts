d61c0c412bf329d737f0d461b793b813b314245b
Fightbackman <fightbackman@firepulse.de>
2016-06-24 14:56:35 +0200

    Added variable port definition for scp

--- ssh-spreadkeys/spread_keys.sh
+++ ssh-spreadkeys/spread_keys.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+port=$1;
+if [ -z $port ]; then
+port=80;
+fi;
 while read p; do
   echo "sending Keys to $p"
-  scp -P 4875 ./authorized_keys $p:~/.ssh/autorized_keys
+  scp -P $port ./authorized_keys $p:~/.ssh/autorized_keys
 done <./to
