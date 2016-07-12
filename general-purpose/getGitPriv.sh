46321828fd8896628bb4e0841973f464d620f83c
Fightbackman <fightbackman@firepulse.de>
2016-07-12 22:24:08 +0200

    added script to download files frpm private github repos

---
+++ general-purpose/getGitPriv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TOKEN=$1;
+OWNER=$2;
+REPO=$3;
+GITPATH=$4; #example bin/sample.txt
+OUTPATH=$5
+FILE="https://api.github.com/repos/$OWNER/$REPO/contents/$GITPATH";
+
+curl --header "Authorization: token $TOKEN" \
+     --header 'Accept: application/vnd.github.v3.raw' \
+     --remote-name \
+     --location $FILE
+     -o $OUTPATH;
