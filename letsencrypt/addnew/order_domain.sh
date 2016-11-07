a622186e1dd5f850d92e594fe8dba08e27d3a9d0
Fightbackman <lukasglitt@aol.com>
2016-11-07 22:23:27 +0100

    updated challenges

--- letsencrypt/addnew/order_domain.sh
+++ letsencrypt/addnew/order_domain.sh
@@ -6,7 +6,7 @@ le_path='/opt/certbot';
 echo "Shutdown of $web_service"
 /usr/sbin/service $web_service stop
 echo "Aquiring new certificate"
-$le_path/certbot-auto certonly --agree-tos --renew-by-default --standalone --standalone-supported-challenges tls-sni-01 --config $config_file
+$le_path/certbot-auto certonly --agree-tos --renew-by-default --standalone --preffered-challenges tls-sni-01,http-01 --config $config_file
 echo "Aquiring for $newdomain was successfull."
 echo "Restarting $web_service"
 /usr/sbin/service $web_service start
