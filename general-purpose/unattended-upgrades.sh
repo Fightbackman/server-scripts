19796cc5f33c3b3885493e851bc7581a02ea0ac0
Fightbackman <lukasglitt@aol.com>
2017-03-23 23:08:16 +0100

    hopefully fixed unattended upgrades.sh

--- general-purpose/unattended-upgrades.sh
+++ general-purpose/unattended-upgrades.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-USER=$1; IP=$2; ssh -t $USER@$IP "
+USER=$1; IP=$2; ssh -t $USER@$IP << EOF
 sudo apt install unattended-upgrades;
 sudo sed -i '71 s/^.//' /etc/apt/apt.conf.d/50unattended-upgrades;
 sudo sed -i '71 s/^.//' /etc/apt/apt.conf.d/50unattended-upgrades;
 sudo touch /etc/apt/apt.conf.d/20auto-upgrades;
-echo "APT::Periodic::Update-Package-Lists "1";" | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades;
-echo "APT::Periodic::Unattended-Upgrade "1";" | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades;
-sudo unattended-upgrade;"
+echo "APT::Periodic::Update-Package-Lists \"1\";" | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades;
+echo "APT::Periodic::Unattended-Upgrade \"1\";" | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades;
+sudo unattended-upgrade;
+EOF
 exit 0;
